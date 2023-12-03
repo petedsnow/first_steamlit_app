ce2bd6bd02f12d7757b1f422c24e8d92531bd72e
petedsnow <152810105+petedsnow@users.noreply.github.com>
2023-12-03 12:16:41 -0700

    Update streamlit_app.py
    Breakfast Menu w/ emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title ('My Parents New Healther Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
